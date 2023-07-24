763f76b92e07b82f3c7572a6f95a1f6c0a9caeac
Lachlan WICK <WIC0015@mwsc.vic.edu.au>
2023-07-24 10:11:20 +1000

    shraban likes cashews

--- Management Interface/MainWindow.xaml.vb
+++ Management Interface/MainWindow.xaml.vb
@@ -1,3 +1,9 @@
 ﻿Class MainWindow
+    Private Sub Password_TextInput(sender As Object, e As TextCompositionEventArgs)
 
+    End Sub
+
+    Private Sub Username_TextInput(sender As Object, e As TextCompositionEventArgs)
+
+    End Sub
 End Class
